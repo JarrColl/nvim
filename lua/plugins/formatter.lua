714cfc52cf45c8a41e2b8ec39f844b47eef3ecb8
JarrColl <15962073+JarrColl@users.noreply.github.com>
2025-11-15 19:16:32 +1100

    update conform formatter setup and add a list of languages to enable format on save

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -1,11 +1,11 @@
 return {
     'stevearc/conform.nvim',
-    lazy = false,
+    cmd = { 'ConformInfo' },
     keys = {
         {
             '<leader>f',
             function()
-                require('conform').format { async = true, lsp_fallback = true }
+                require('conform').format { async = true, lsp_format = 'fallback' }
             end,
             mode = '',
             desc = '[F]ormat buffer',
@@ -13,26 +13,28 @@ return {
     },
     opts = {
         notify_on_error = false,
-        --[[ format_on_save = function(bufnr)
-        -- Disable "format_on_save lsp_fallback" for languages that don't
-        -- have a well standardized coding style. You can add additional
-        -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
-        return {
-          timeout_ms = 700,
-          lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
-        }
-        end, ]]
+        format_on_save = function(bufnr)
+            local enable_filetypes = { lua = true }
+            if enable_filetypes[vim.bo[bufnr].filetype] then
+                return {
+                    timeout_ms = 500,
+                    lsp_format = 'fallback',
+                }
+            else
+                return nil
+            end
+        end,
         formatters_by_ft = {
             lua = { 'stylua' },
             --python = { 'ruff' },
             python = { 'isort', 'black' },
             javascript = { 'prettierd' },
+            -- javascript = { 'prettierd', 'prettier', stop_after_first = true },
             css = { 'prettierd' },
             html = { 'prettierd' },
             json = { 'prettierd' },
             yaml = { 'prettierd' },
-            xml = { 'xmlformatter' }
+            xml = { 'xmlformatter' },
         },
     },
 }
