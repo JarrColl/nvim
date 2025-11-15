fef99e3f45b2689ee830682071832432bbad8354
JarrColl <15962073+JarrColl@users.noreply.github.com>
2025-11-15 19:13:42 +1100

    copy new lazy.nvim setup from kickstart, move whichkey to its own plugin file

--- lua/config/lazyInit.lua
+++ lua/config/lazyInit.lua
@@ -1,11 +1,15 @@
--- TODO: Add the buffers as visual tabs on top?
-
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
     local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-    vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
-end ---@diagnostic disable-next-line: undefined-field
-vim.opt.rtp:prepend(lazypath)
+    local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+    if vim.v.shell_error ~= 0 then
+        error('Error cloning lazy.nvim:\n' .. out)
+    end
+end
+
+---@type vim.Option
+local rtp = vim.opt.rtp
+rtp:prepend(lazypath)
 
 require('lazy').setup({
     'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
@@ -30,68 +34,6 @@ require('lazy').setup({
     -- Then, because we use the `config` key, the configuration only runs
     -- after the plugin has been loaded:
     --  config = function() ... end
-
-    { -- Useful plugin to show you pending keybinds.
-        'folke/which-key.nvim',
-        event = 'VimEnter', -- Sets the loading event to 'VimEnter'
-        opts = {
-            delay = 0,
-            icons = {
-                -- set icon mappings to true if you have a Nerd Font
-                mappings = vim.g.have_nerd_font,
-
-                -- If you are using a Nerd Font: set icons.keys to an empty table which will use the
-                -- default which-key.nvim defined Nerd Font icons, otherwise define a string table
-                keys = vim.g.have_nerd_font and {} or {
-                    Up = '<Up> ',
-                    Down = '<Down> ',
-                    Left = '<Left> ',
-                    Right = '<Right> ',
-                    C = '<C-…> ',
-                    M = '<M-…> ',
-                    D = '<D-…> ',
-                    S = '<S-…> ',
-                    CR = '<CR> ',
-                    Esc = '<Esc> ',
-                    ScrollWheelDown = '<ScrollWheelDown> ',
-                    ScrollWheelUp = '<ScrollWheelUp> ',
-                    NL = '<NL> ',
-                    BS = '<BS> ',
-                    Space = '<Space> ',
-                    Tab = '<Tab> ',
-                    F1 = '<F1>',
-                    F2 = '<F2>',
-                    F3 = '<F3>',
-                    F4 = '<F4>',
-                    F5 = '<F5>',
-                    F6 = '<F6>',
-                    F7 = '<F7>',
-                    F8 = '<F8>',
-                    F9 = '<F9>',
-                    F10 = '<F10>',
-                    F11 = '<F11>',
-                    F12 = '<F12>',
-                },
-            },
-        },
-        spec = {
-            { '<leader>c', group = '[C]ode' },
-            { '<leader>c_', hidden = true },
-            { '<leader>d', group = '[D]ocument' },
-            { '<leader>d_', hidden = true },
-            { '<leader>h', group = 'Git [H]unk' },
-            { '<leader>h_', hidden = true },
-            { '<leader>r', group = '[R]ename' },
-            { '<leader>r_', hidden = true },
-            { '<leader>s', group = '[S]earch' },
-            { '<leader>s_', hidden = true },
-            { '<leader>t', group = '[T]oggle' },
-            { '<leader>t_', hidden = true },
-            { '<leader>w', group = '[W]orkspace' },
-            { '<leader>w_', hidden = true },
-            { '<leader>h', desc = 'Git [H]unk', mode = 'v' },
-        },
-    },
 
     -- Highlight todo, notes, etc in comments
     { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
