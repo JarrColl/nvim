4e3ab4d015ce83e8c9bf4b57c7de4da727556bbe
JarrColl <15962073+JarrColl@users.noreply.github.com>
2025-12-11 19:28:43 +1100

    oil cleanup

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -2,31 +2,30 @@ return {
     'stevearc/oil.nvim',
     ---@module 'oil'
     ---@type oil.SetupOpts
-    config = function()
-        require('oil').setup {
-            columns = { 'icon' },
-            keymaps = {
-                ['L'] = 'actions.select',
-                ['H'] = 'actions.parent',
-                ['gd'] = {
-                    desc = 'Toggle file detail view',
-                    callback = function()
-                        detail = not detail
-                        if detail then
-                            require('oil').set_columns { 'icon', 'permissions', 'size', 'mtime' }
-                        else
-                            require('oil').set_columns { 'icon' }
-                        end
-                    end,
-                },
-                ['\\'] = { 'actions.close', mode = 'n' },
+    opts = {
+        columns = { 'icon' },
+        keymaps = {
+            ['L'] = 'actions.select',
+            ['H'] = 'actions.parent',
+            ['gd'] = {
+                desc = 'Toggle file detail view',
+                callback = function()
+                    detail = not detail
+                    if detail then
+                        require('oil').set_columns { 'icon', 'permissions', 'size', 'mtime' }
+                    else
+                        require('oil').set_columns { 'icon' }
+                    end
+                end,
             },
-        }
-        -- Open parent directory in current window
-        vim.keymap.set('n', '\\', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
-    end,
-    opts = {},
+            ['\\'] = { 'actions.close', mode = 'n' },
+        },
+    },
+    keys = {
+        { '\\', '<CMD>Oil<CR>', { desc = 'Open parent directory' } },
+    },
+    lazy = false,
     -- Optional dependencies
-    dependencies = { { 'echasnovski/mini.icons', opts = {} } },
-    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+    -- dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+    dependencies = { 'nvim-tree/nvim-web-devicons' }, -- use if prefer nvim-web-devicons
 }
