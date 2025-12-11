98980ca7e72e284f14801a2f10489940ee10d3bd
JarrColl <15962073+JarrColl@users.noreply.github.com>
2025-12-11 18:41:15 +1100

    comment luasnip

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -5,31 +5,31 @@ return {
     version = '1.*', -- NOTE: Update this occasionally to recieve major version updates. LuaSnip too.
 
     dependencies = {
-        {
-            'L3MON4D3/LuaSnip',
-            version = '2.*',
-            build = (function()
-                -- Build Step is needed for regex support in snippets.
-                -- This step is not supported in many windows environments.
-                -- Remove the below condition to re-enable on windows.
-                if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-                    return
-                end
-                return 'make install_jsregexp'
-            end)(),
-            dependencies = {
-                -- `friendly-snippets` contains a variety of premade snippets.
-                --    See the README about individual language/framework/plugin snippets:
-                --    https://github.com/rafamadriz/friendly-snippets
-                {
-                    'rafamadriz/friendly-snippets',
-                    config = function()
-                        require('luasnip.loaders.from_vscode').lazy_load()
-                    end,
-                },
-            },
-            opts = {},
-        },
+        -- {
+        --     'L3MON4D3/LuaSnip',
+        --     version = '2.*',
+        --     build = (function()
+        --         -- Build Step is needed for regex support in snippets.
+        --         -- This step is not supported in many windows environments.
+        --         -- Remove the below condition to re-enable on windows.
+        --         if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
+        --             return
+        --         end
+        --         return 'make install_jsregexp'
+        --     end)(),
+        --     dependencies = {
+        --         -- `friendly-snippets` contains a variety of premade snippets.
+        --         --    See the README about individual language/framework/plugin snippets:
+        --         --    https://github.com/rafamadriz/friendly-snippets
+        --         {
+        --             'rafamadriz/friendly-snippets',
+        --             config = function()
+        --                 require('luasnip.loaders.from_vscode').lazy_load()
+        --             end,
+        --         },
+        --     },
+        --     opts = {},
+        -- },
         'folke/lazydev.nvim',
     },
 
@@ -71,7 +71,7 @@ return {
             -- optionally disable cmdline completions
             -- cmdline = {},
         },
-        snippets = { preset = 'luasnip' },
+        -- snippets = { preset = 'luasnip' },
 
         signature = { enabled = true },
     },
