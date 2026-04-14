14df2dc8e3f156f71d266b8986031a551f907824
JarrColl <15962073+JarrColl@users.noreply.github.com>
2026-04-14 23:13:59 +1000

    organise options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,18 +3,14 @@
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
 
--- Make line numbers default
-vim.o.number = true
+vim.o.number = true -- Make line numbers default
 vim.o.relativenumber = true
-
--- Enable mouse mode, can be useful for resizing splits for example!
-vim.o.mouse = 'a'
-
--- Don't show the mode, since it's already in the status line
-vim.o.showmode = false
-
-
+vim.o.cursorline = true -- Show which line your cursor is on
 vim.o.wrap = false
+vim.o.scrolloff = 10
+vim.o.sidescrolloff = 8
+
+vim.o.mouse = 'a'
 
 -- Turn off insert comment on new line.
 -- vim.cmd "set formatoptions-=cro"
@@ -22,40 +18,36 @@ vim.o.wrap = false
 -- vim.opt.formatoptions:remove "r"
 -- -vim.opt.formatoptions:remove "o"
 
--- Enable break indent for wrapped text
+-- ########### Indentation
 vim.o.breakindent = true
-
+vim.o.linebreak = true
 vim.o.tabstop = 4
 vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
 vim.o.expandtab = false
-
 -- vim.opt.autoindent = true
 vim.o.smartindent = true
 
--- Replace swap files with undo history
-vim.o.swapfile = false
+-- # Files
+vim.o.swapfile = false -- Replace swap files with undo history
 vim.o.backup = false
 
--- Save undo history
+vim.o.undofile = true -- Save undo history
 vim.o.undodir = os.getenv 'HOME' .. '/.vim/undodir'
-vim.o.undofile = true
+vim.o.autoread = true -- If a file has changed and there are no unsaved changes, auto read it without a warning.
 
--- If a file has changed and there are no unsaved changes, auto read it without a warning.
-vim.o.autoread = true
+-- # Search Settings
+vim.o.ignorecase = true -- Case-insensitive searching
+vim.o.smartcase = true -- UNLESS \C or one or more capital letters in the search term.
+vim.opt.hlsearch = true -- Highlight all search results. These are on by default.
+vim.opt.incsearch = true
 
--- Highlight all search results. These are on by default.
--- vim.opt.hlsearch = true
--- vim.opt.incsearch = true
-
--- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
-vim.o.ignorecase = true
-vim.o.smartcase = true
-
+-- # Visual Settings
 vim.o.termguicolors = true
-
--- Keep signcolumn on by default
 vim.o.signcolumn = 'yes'
+vim.o.showmode = false -- Don't show the mode, since it's already in the status line
+vim.o.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
 -- Decrease update time
 vim.o.updatetime = 250
@@ -64,26 +56,12 @@ vim.o.updatetime = 250
 -- Displays which-key popup sooner
 vim.o.timeoutlen = 300
 
--- Configure how new splits should be opened
 vim.o.splitright = true
 vim.o.splitbelow = true
-
--- Sets how neovim will display certain whitespace characters in the editor.
---  See `:help 'list'`
---  and `:help 'listchars'`
-vim.o.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
 -- Preview substitutions live, as you type!
 vim.o.inccommand = 'split'
 
--- Show which line your cursor is on
-vim.o.cursorline = true
-
--- Minimal number of screen lines to keep above and below the cursor.
-vim.o.scrolloff = 10
-
 -- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
 -- instead raise a dialog asking if you wish to save the current file(s)
--- See `:help 'confirm'`
 vim.o.confirm = true
