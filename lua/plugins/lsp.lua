557043c8fb2c0110202229ec7960eeb0cfee91fd
jarron.collins <jarron.collins@planetinnovation.com.au>
2026-06-19 19:25:19 +1000

    Fix vue lsp support

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -169,11 +169,34 @@ return { -- LSP Configuration & Plugins
                                     name = '@vue/typescript-plugin',
                                     location = vim.fn.stdpath 'data' .. '/mason/packages/vue-language-server/node_modules/@vue/language-server',
                                     languages = { 'vue' },
+                                    configNamespace = 'typescript',
                                 },
                             },
                         },
                     },
                 },
+            },
+            vue_ls = {
+                on_init = function(client)
+                    client.handlers['tsserver/request'] = function(_, result, context)
+                        local clients = vim.lsp.get_clients { bufnr = context.bufnr, name = 'vtsls' }
+                        if #clients == 0 then
+                            vim.notify('Could not find vtsls client, vue_ls requires it.', vim.log.levels.ERROR)
+                            return
+                        end
+                        local ts_client = clients[1]
+                        local param = unpack(result)
+                        local id, command, payload = unpack(param)
+                        ts_client:exec_cmd({
+                            title = 'vue_request_forward',
+                            command = 'typescript.tsserverRequest',
+                            arguments = { command, payload },
+                        }, { bufnr = context.bufnr }, function(_, r)
+                            local response_data = { { id, r and r.body } }
+                            client:notify('tsserver/response', response_data)
+                        end)
+                    end
+                end,
             },
             arduino_language_server = {
                 settings = {
